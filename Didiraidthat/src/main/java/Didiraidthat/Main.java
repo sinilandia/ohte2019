24931d7b1ce891c81038a0e2788695fbdfaebdb1
Sini Liu <aisinilandia@gmail.com>
2019-04-09 12:52:05 +0300

    Update Main.java

--- Didiraidthat/src/main/java/Didiraidthat/Main.java
+++ Didiraidthat/src/main/java/Didiraidthat/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package didiraidthat;
+package Didiraidthat;
 
 import java.util.ArrayList;
 import java.util.stream.Collectors;
