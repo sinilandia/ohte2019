5b3445566568be43b36f3498a4ad0616d33741ad
sinilandia <aisinilandia@gmail.com>
2019-04-14 14:51:50 +0300

    refactor and add gym and raid classes

--- Didiraidthat/src/main/java/domain/Gym.java
+++ Didiraidthat/src/main/java/domain/Gym.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package domain;
 
 /**
@@ -10,5 +5,57 @@ package domain;
  * @author siniliu
  */
 public class Gym {
+    private int id;
+    private String name;
+    private boolean EX;
+
+    public Gym(String name, boolean EX) {
+        this.name = name;
+        this.EX = EX;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isEX() {
+        return EX;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEX(boolean EX) {
+        this.EX = EX;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        
+        final Gym other = (Gym) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+            
+           
+    
+ 
     
 }
--- Didiraidthat/src/main/java/domain/Raid.java
+++ Didiraidthat/src/main/java/domain/Raid.java
@@ -5,10 +5,98 @@
  */
 package domain;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 /**
  *
  * @author siniliu
  */
 public class Raid {
+    private int id;
+    private String level;
+    private Gym gym;
+    private LocalDate date;
+    private LocalTime time;
+    private boolean raided;
+    //add participants  and raid boss
+
+    public Raid(String level, Gym gym, LocalDate date, LocalTime time) {
+        this.level = level;
+        this.gym = gym;
+        this.date = date;
+        this.time = time;
+        this.raided = false;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public void setGym(Gym gym) {
+        this.gym = gym;
+    }
+
+    public void setRaided(boolean raided) {
+        this.raided = raided;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public Gym getGym() {
+        return gym;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public boolean isRaided() {
+        return raided;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Raid other = (Raid) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
     
 }
